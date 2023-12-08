d1c06b13ece3fb24786f983502c1c8530d8daafc
sulfikaralijun <sulfikaralijun@gmail.com>
2023-12-08 16:58:38 +0800

    fix logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <header className="absolute w-full left-0 px-10 py-5 flex items-center justify-between backdrop-blur-sm">
         <a href="#" className="flex gap-3 items-center">
-          <img src="/src/assets/image/Logo.png" alt="Logo" className="w-10 rounded-full" />
+          <img src="../assets/image/Logo.png" alt="Logo" className="w-10 rounded-full" />
           <h1 className="font-mono font-semibold text-[1.2rem] text-slate-800">Sulfikar Alijun</h1>
         </a>
         <nav className="flex gap-3 bg-slate" >
